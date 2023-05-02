1231aa21d8695741fd57db9825b02a12727f527f
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-05-02 15:00:15 -0400

    Update main.tf

--- sensitive/main.tf
+++ sensitive/main.tf
@@ -5,5 +5,9 @@ resource "random_password" "example" {
 
 output "password" {
   value = random_password.example.result
-  sensitive = false
+  sensitive = true
+}
+
+output "message" {
+  value = "Your deployment is complete."
 }
