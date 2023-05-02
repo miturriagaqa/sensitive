ea2ae730d181a9712d4a9712443cc497ad59bea4
miturriagaqa <109548388+miturriagaqa@users.noreply.github.com>
2023-05-02 15:31:49 -0400

    Update variables.tf

--- sensitive/variables.tf
+++ sensitive/variables.tf
@@ -7,3 +7,13 @@ variable "secret_key" {
   description = "Value of the Secret Key for AWS"
   type        = string
 }
+
+variable "my_secret_key" {
+  type    = string
+  default = "defaultsecretkey"
+}
+
+output "secret_key_output" {
+  value     = var.my_secret_key
+  sensitive = true
+}
